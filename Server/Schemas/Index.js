3fcb7147cbb6393145209bab49973642d1bd9af2
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-04-03 02:50:04 +0100

    Chief Cooker is cooking 👨🏾‍🍳

--- Server/Schemas/Index.js
+++ Server/Schemas/Index.js
@@ -1,1 +1,7 @@
-export const schemaTypes = []
+import Comment from "./Comment"
+import Post from "./Post"
+import PostedBy from "./PostedBy"
+import Save from "./Save"
+import User from "./User"
+
+export const schemaTypes = [ Comment, Post, PostedBy, Save, User ]
