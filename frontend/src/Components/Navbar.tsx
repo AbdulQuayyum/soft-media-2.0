de2cdcaf332b5dc3c7669342b964388443b933d6
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-04-04 18:45:17 +0100

    Cooking in progress, but not a Chef👨🏾‍🍳

--- frontend/src/Components/Navbar.tsx
+++ frontend/src/Components/Navbar.tsx
@@ -7,9 +7,15 @@ import { BiSearch } from 'react-icons/bi'
 import { IoMdAdd } from 'react-icons/io'
 import { GoogleLogin, googleLogout } from '@react-oauth/google'
 
+import { CreateOrGetUser } from '../Utilities/Index'
+import { IUser } from '../../types'
 import Logo from '../Utilities/logo.png'
 
 const Navbar = () => {
+  const [user, setUser] = useState<IUser | null>()
+  const [searchValue, setSearchValue] = useState('')
+  const router = useRouter()
+
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
@@ -22,6 +28,66 @@ const Navbar = () => {
           />
         </div>
       </Link>
+      <div className="relative hidden md:block">
+        <form
+          // onSubmit={handleSearch}
+          className="absolute md:static top-10 -left-20 bg-white"
+        >
+          <input
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            className="bg-primary p-3 md:text-md font-medium border-2 border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 w-[300px] md:w-[350px] rounded-full  md:top-0"
+            placeholder="Search accounts and videos"
+          />
+          <button
+            // onClick={handleSearch}
+            className="absolute md:right-5 right-6 top-4 border-l-2 border-gray-300 pl-4 text-2xl text-gray-400"
+          >
+            <BiSearch />
+          </button>
+        </form>
+      </div>
+      <div>
+        {user ? (
+          <div className="flex gap-5 md:gap-10">
+            <Link href="/Upload">
+              <button className="border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2">
+                <IoMdAdd className="text-xl" />{' '}
+                <span className="hidden md:block">Upload </span>
+              </button>
+            </Link>
+            {user.Image && (
+              <Link href={`/Profile/${user._id}`}>
+                <div>
+                  <Image
+                    className="rounded-full cursor-pointer"
+                    src={user.Image}
+                    alt="user"
+                    width={40}
+                    height={40}
+                    referrerPolicy="no-referrer"
+                  />
+                </div>
+              </Link>
+            )}
+            <button
+              type="button"
+              className=" border-2 p-2 rounded-full cursor-pointer outline-none shadow-md"
+              onClick={() => {
+                googleLogout()
+                // removeUser();
+              }}
+            >
+              <AiOutlineLogout color="red" fontSize={21} />
+            </button>
+          </div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(response) => console.log(response)}
+            onError={() => console.log('Login Failed')}
+          />
+        )}
+      </div>
     </div>
   )
 }
--- frontend/src/Utilities/Client.ts
+++ frontend/src/Utilities/Client.ts
@@ -4,7 +4,7 @@ export const Client = createClient({
   apiVersion: '2023-04-03',
   dataset: 'production',
   ignoreBrowserTokenWarning: true,
-  token: process.env.NEXT_SANITY_TOKEN,
-  projectId: process.env.NEXT_SANITY_PROJECTID,
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECTID,
   useCdn: true,
 })
