e0fb196f96705d74b41d0391b93f2eeb8493d361
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-04-04 16:53:30 +0100

    Cooking in progress, but not a Chef👨🏾‍🍳

--- frontend/src/Components/VideoCard.tsx
+++ frontend/src/Components/VideoCard.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { NextPage } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
+import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi'
+import { BsFillPlayFill, BsFillPauseFill } from 'react-icons/bs'
+import { GoVerified } from 'react-icons/go'
+import { BsPlay } from 'react-icons/bs'
 
-const VideoCard = () => {
-  return (
-    <div>VideoCard</div>
-  )
+import { Video } from '../../types'
+
+interface IProps {
+  post: Video
+  isShowingOnHome?: boolean
+}
+
+const VideoCard: NextPage<IProps> = ({
+  post: { Caption, PostedBy, Video, _id, Likes },
+  isShowingOnHome,
+}) => {
+  return <div>VideoCard</div>
 }
 
 export default VideoCard
