0b3f754aec2279ddd41b7d51d06c4a2ed1e00fcf
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-04-07 13:41:04 +0100

    Cooking, but not a Chef👨🏾‍🍳

---
+++ frontend/src/pages/Profile/[UserID].tsx
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+import { GoVerified } from 'react-icons/go';
+import axios from 'axios';
+
+import { BASE_URL } from '../../Utilities/Index';
+import { IUser, Video } from '../../../types';
+import { NoResults, VideoCard } from "../../Components/Index"
+
+interface IProps {
+    data: {
+      User: IUser;
+      UserVideos: Video[];
+      UserLikedVideos: Video[];
+    };
+  }
+  
+  const Profile = ({ data }: IProps) => {
+    const [showUserVideos, setShowUserVideos] = useState<Boolean>(true);
+    const [videosList, setVideosList] = useState<Video[]>([]);
+  
+    const { User, UserVideos, UserLikedVideos } = data;
+    const videos = showUserVideos ? 'border-b-2 border-black' : 'text-gray-400';
+    const liked = !showUserVideos ? 'border-b-2 border-black' : 'text-gray-400';
+  
+    useEffect(() => {
+      const fetchVideos = async () => {
+        if (showUserVideos) {
+          setVideosList(UserVideos);
+        } else {
+          setVideosList(UserLikedVideos);
+        }
+      };
+  
+      fetchVideos();
+    }, [showUserVideos, UserLikedVideos, UserVideos]);
+  
+    return (
+      <div className='w-full'>
+        <div className='flex gap-6 md:gap-10 mb-4 bg-white w-full'>
+          <div className='w-16 h-16 md:w-32 md:h-32'>
+            <Image
+              width={120}
+              height={120}
+              className='rounded-full'
+              src={User.Image}
+              alt='User-profile'
+            />
+          </div>
+  
+          <div>
+            <div className='text-md md:text-2xl font-bold tracking-wider flex gap-2 items-center justify-center lowercase'>
+              <span>{User.UserName.replace(/\s+/g, '')} </span>
+              <GoVerified className='text-blue-400 md:text-xl text-md' />
+            </div>
+            <p className='text-sm font-medium'> {User.UserName}</p>
+          </div>
+        </div>
+        <div>
+          <div className='flex gap-10 mb-10 mt-10 border-b-2 border-gray-200 bg-white w-full'>
+            <p className={`text-xl font-semibold cursor-pointer ${videos} mt-2`} onClick={() => setShowUserVideos(true)}>
+              Videos
+            </p>
+            <p className={`text-xl font-semibold cursor-pointer ${liked} mt-2`} onClick={() => setShowUserVideos(false)}>
+              Liked
+            </p>
+          </div>
+          <div className='flex gap-6 flex-wrap md:justify-start'>
+            {videosList.length > 0 ? (
+              videosList.map((post: Video, idx: number) => (
+                <VideoCard key={idx} Post={post} />
+              ))
+            ) : (
+              <NoResults
+                text={`No ${showUserVideos ? '' : 'Liked'} Videos Yet`}
+              />
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export const getServerSideProps = async ({
+    params: { UserID },
+  }: {
+    params: { UserID: string };
+  }) => {
+    const res = await axios.get(`${BASE_URL}/api/Profile/${UserID}`);
+  
+    return {
+      props: { data: res.data },
+    };
+  };
+  export default Profile;
---
+++ frontend/src/pages/Search/[searchTerm].tsx
@@ -0,0 +1,101 @@
+import React, { useState } from 'react'
+import Image from 'next/image'
+import { useRouter } from 'next/router'
+import { GoVerified } from 'react-icons/go'
+import Link from 'next/link'
+import axios from 'axios'
+
+import { BASE_URL } from '../../Utilities/Index'
+import { IUser, Video } from '../../../types'
+import { NoResults, VideoCard } from '../../Components//Index'
+import UseAuthStore from '../../Store/AuthStore'
+
+const Search = ({ Videos }: { Videos: Video[] }) => {
+  const [isAccounts, setIsAccounts] = useState(false)
+  const { AllUsers }: { AllUsers: IUser[] } = UseAuthStore()
+
+  const router = useRouter()
+  const { searchTerm }: any = router.query
+
+  const accounts = isAccounts ? 'border-b-2 border-black' : 'text-gray-400'
+  const isVideos = !isAccounts ? 'border-b-2 border-black' : 'text-gray-400'
+  const searchedAccounts = AllUsers?.filter((User: IUser) =>
+    User.UserName.toLowerCase().includes(searchTerm),
+  )
+
+  return (
+    <div className="w-full  ">
+      <div className="flex gap-10 mb-10 border-b-2 border-gray-200 md:fixed z-50 bg-white w-full">
+        <p
+          onClick={() => setIsAccounts(true)}
+          className={`text-xl  font-semibold cursor-pointer ${accounts} mt-2`}
+        >
+          Accounts
+        </p>
+        <p
+          className={`text-xl font-semibold cursor-pointer ${isVideos} mt-2`}
+          onClick={() => setIsAccounts(false)}
+        >
+          Videos
+        </p>
+      </div>
+      {isAccounts ? (
+        <div className="md:mt-16">
+          {searchedAccounts.length > 0 ? (
+            searchedAccounts.map((User: IUser, idx: number) => (
+              <Link key={idx} href={`/Profile/${User._id}`}>
+                <div className=" flex gap-3 p-2 cursor-pointer font-semibold rounded border-b-2 border-gray-200">
+                  <div>
+                    <Image
+                      width={50}
+                      height={50}
+                      className="rounded-full"
+                      alt="User-profile"
+                      src={User.Image}
+                    />
+                  </div>
+                  <div>
+                    <div>
+                      <p className="flex gap-1 items-center text-lg font-bold text-primary">
+                        {User.UserName} <GoVerified className="text-blue-400" />
+                      </p>
+                      <p className="capitalize text-gray-400 text-sm">
+                        {User.UserName}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            ))
+          ) : (
+            <NoResults text={`No Account Results for ${searchTerm}`} />
+          )}
+        </div>
+      ) : (
+        <div className="md:mt-16 flex flex-wrap gap-6 md:justify-start ">
+          {Videos.length ? (
+            Videos.map((Post: Video, idx: number) => (
+              <VideoCard Post={Post} key={idx} />
+            ))
+          ) : (
+            <NoResults text={`No Video Results for ${searchTerm}`} />
+          )}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export const getServerSideProps = async ({
+  params: { searchTerm },
+}: {
+  params: { searchTerm: string }
+}) => {
+  const res = await axios.get(`${BASE_URL}/api/Search/${searchTerm}`)
+
+  return {
+    props: { Videos: res.data },
+  }
+}
+
+export default Search
